06037d0c161ced94422cfcb436d2f164a34969b7
Núria Ramoneda <nuriarai@gmail.com>
2023-04-05 20:49:08 +0200

    Add sorting basic funcitinality, add testing data to html

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,27 @@ toggleView.addEventListener("change", (e) => {
   toggleView.setAttribute("aria-checked", toggleView.checked);
   document.querySelector(".widget__list").classList.toggle("widget__cards");
 });
+
+/* sorting */
+
+const toggleSort = document.querySelector(".widget__sort-switcher");
+
+toggleSort.addEventListener("click", (e) => {
+  const container = document.querySelector(".widget__list");
+
+  container.classList.toggle("alfabetical");
+
+  if (container.classList.contains("alfabetical")) {
+    toggleSort.textContent = "Sort by position";
+    let key = (a) => a.querySelector(".item__title").textContent.trim();
+    Array.from(container.children)
+      .sort((a, b) => key(a).localeCompare(key(b)))
+      .forEach((child) => container.appendChild(child));
+  } else {
+    toggleSort.textContent = "Sort Alphabetically";
+    let key = (a) => a.querySelector(".item__order").textContent.trim();
+    Array.from(container.children)
+      .sort((a, b) => key(a).localeCompare(key(b)))
+      .forEach((child) => container.appendChild(child));
+  }
+});
